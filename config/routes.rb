1502ee1e864c2f971ee9244867379b5f16f1e55e
sapsford <craig.sapsford11@gmail.com>
2016-04-20 13:35:10 +0200

    Add comments to posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_for :models
-  resources :posts
+  resources :posts do
   	resources :comments
   end
 
-  root "posts#index"
+  root 'posts#index'
 end
